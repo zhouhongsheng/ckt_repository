49e74d8970a9c858722081cff6a4357235de022b
jinx.zhou <jinx.zhou@ck-telecom.com>
2017-10-11 14:10:38 +0800

    remove tran && add eureka

---
+++ eureka_server/src/main/java/com/sioeye/eureka/EurekaApp.java
@@ -0,0 +1,20 @@
+package com.sioeye.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 
+ * @author zhouyou
+ * @date 2017年10月11日
+ * @desc eureka services
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApp {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApp.class, args);
+		System.out.println("start eureka server success .");
+	}
+}
--- tran/src/main/java/tran/tran/common/StaticConstant.java
+++
@@ -1,6 +0,0 @@
-package tran.tran.common;
-
-public class StaticConstant {
-
-	public static final int PRE_COLUMNNAME_NUM=1;
-}
